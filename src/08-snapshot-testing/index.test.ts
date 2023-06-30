9723911896530e9eeb18d0313f1e006c06a90b10
Yuri Skripal <yjura@protonmail.com>
2023-06-30 11:56:33 +0300

    feat: add 08 task

--- src/08-snapshot-testing/index.test.ts
+++ src/08-snapshot-testing/index.test.ts
@@ -1,14 +1,29 @@
-// Uncomment the code below and write your tests
-// import { generateLinkedList } from './index';
+import { generateLinkedList } from './index';
 
 describe('generateLinkedList', () => {
+  const elements = [1, 2, 3, 4];
+  const linkedList = {
+    value: 1,
+    next: {
+      value: 2,
+      next: {
+        value: 3,
+        next: {
+          value: 4,
+          next: { value: null, next: null },
+        },
+      },
+    },
+  };
   // Check match by expect(...).toStrictEqual(...)
   test('should generate linked list from values 1', () => {
-    // Write your test here
+    expect(generateLinkedList(elements)).toStrictEqual(linkedList);
   });
 
   // Check match by comparison with snapshot
   test('should generate linked list from values 2', () => {
-    // Write your test here
+    const result = generateLinkedList(elements);
+
+    expect(result).toMatchSnapshot();
   });
 });
